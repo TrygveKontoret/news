13661af241c187f1048c3b871f79479c6111a80b
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-08-31 13:05:59 +0200

    removed bloat, added article and header.

---
+++ src/components/article/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Article = () => {
+    return (
+        <div>
+            <h2>Headline</h2>
+            <p>Body</p>
+            <span>Author:</span>
+        </div>
+    )
+}
+
+export default Article
